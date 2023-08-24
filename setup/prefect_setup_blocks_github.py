8bceaaaea3e1317da7b6fb9ada8c75e28b8dec7e
mikecolemn <107276566+mikecolemn@users.noreply.github.com>
2023-08-23 20:56:28 -0500

    Delete prefect_setup_blocks_github.py

--- setup/prefect_setup_blocks_github.py
+++
@@ -1,9 +0,0 @@
-from prefect.filesystems import GitHub
-
-block = GitHub(
-    repository="https://github.com/mikecolemn/mpls-311-data/",
-    reference="dev" # Can refer to a branch
-    #access_token=<my_access_token> # only required for private repos
-)
-block.get_directory("flows") # specify a subfolder of repo
-block.save("gh-mpls311", overwrite=True)
